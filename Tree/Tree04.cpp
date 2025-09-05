cfdadbe46d55a80ae57d0f608aef16070521e655
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2025-09-05 22:12:13 +0500

    Maximum depth of binary tree

---
+++ Tree/Tree04.cpp
@@ -0,0 +1,33 @@
+//Maximum depth of binary tree
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    cout << "Maximum Depth: " << sol.maxDepth(root) << endl;
+
+    return 0;
+}
