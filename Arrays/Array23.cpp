a7572348951d14360b374a773481ed82f51f0728
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2025-09-08 22:36:10 +0500

    Missing number

---
+++ Arrays/Array23.cpp
@@ -0,0 +1,33 @@
+//Missing number
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums1 = {3, 0, 1};
+    vector<int> nums2 = {0, 1};
+    vector<int> nums3 = {9,6,4,2,3,5,7,0,1};
+
+    cout << sol.missingNumber(nums1) << endl;  // 2
+    cout << sol.missingNumber(nums2) << endl;  // 2
+    cout << sol.missingNumber(nums3) << endl;  // 8
+
+    return 0;
+}
